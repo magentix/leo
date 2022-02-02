b02d4856533b5c51b06d5fb9578e23100c3cb2a9
magentix <mvion@magentix.fr>
2022-02-02 11:49:48 +0100

    Script refactoring

--- README.md
+++ README.md
@@ -4,12 +4,12 @@ Leo is the smallest Gemini server in the universe.
 
 # Requirements
 
-- PHP >= 7.4
+- PHP >= 8.0
 - OpenSSL >= 1.1.1
 
 # Configuration
 
-Generate a self-signed certificate with **gemini** as passphrase:
+Generate a self-signed certificate with **gemini** as passphrase and your server FQDN as common name:
 
 ```bash
 openssl req -x509 -newkey rsa:4096 -keyout key.pem -out cert.pem -days 365
--- leo.php
+++ leo.php
@@ -1,38 +1,60 @@
 <?php
 
-$root = __DIR__ . '/';
+const ROOT = __DIR__ . '/';
 
-$socket = stream_socket_server(
-    'tcp://0:1965',
-    $ec,
-    $em,
-    STREAM_SERVER_BIND|STREAM_SERVER_LISTEN,
-    stream_context_create(
-        [
-            'ssl' => [
-                'local_cert' => $root . 'cert.pem',
-                'local_pk' => $root . 'key.pem',
-                'passphrase' => 'gemini',
-                'allow_self_signed' => true,
-                'verify_peer' => false,
-            ]
+$resource = stream_context_create(
+    [
+        'ssl' => [
+            'local_cert' => ROOT . 'cert.pem',
+            'local_pk' => ROOT . 'key.pem',
+            'passphrase' => 'gemini',
+            'allow_self_signed' => true,
+            'verify_peer' => false,
         ]
-    )
+    ]
 );
-
+$socket = stream_socket_server(address: 'tcp://0:1965', context: $resource);
 stream_socket_enable_crypto($socket, false);
 
 while (true) {
-    $fSocket = stream_socket_accept($socket, '-1', $remote);
+    $fSocket = stream_socket_accept($socket, -1);
 
     stream_set_blocking($fSocket, true);
-    stream_socket_enable_crypto($fSocket, true, STREAM_CRYPTO_METHOD_TLSv1_3_SERVER);
+    @stream_socket_enable_crypto($fSocket, true, STREAM_CRYPTO_METHOD_TLSv1_3_SERVER);
     $url = parse_url(trim(fread($fSocket, 1024)));
     stream_set_blocking($fSocket, false);
 
-    $file  = $root . 'capsule' . str_replace('../', '', ($url['path'] ?? '/index.gmi'));
-    $isGmi = substr($file, -3) === 'gmi';
-
-    fwrite($fSocket, $isGmi && is_file($file) ? "20 text/gemini\r\n" . file_get_contents($file) : "51 Not found\r\n");
+    fwrite($fSocket, getContent($url));
     fclose($fSocket);
+}
+
+/**
+ * Retrieve the response content
+ *
+ * @param string[] $url
+ * $url = [
+ *     'scheme' => (string) scheme name (gemini)
+ *     'host'   => (string) host name (gemini.circumlunar.space)
+ *     'path'   => (string) requested page (/about.gmi)
+ * ]
+ *
+ * @return string
+ */
+function getContent(array $url): string
+{
+    if (($url['scheme'] ?? '') !== 'gemini') {
+        return "53 Proxy Request Refused\r\n";
+    }
+
+    $path = $url['path'] ?? '/';
+    if (str_ends_with($path, '/')) {
+        $path .= 'index.gmi';
+    }
+    $file = ROOT . 'capsule' . str_replace('../', '', $path);
+
+    if (!str_ends_with($file, 'gmi') || !file_exists($file)) {
+        return "51 Not found\r\n";
+    }
+
+    return "20 text/gemini\r\n" . file_get_contents($file);
 }
